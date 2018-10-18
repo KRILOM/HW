1d1f9c2ad66b94d79634720f3793e7a7e8574111
KRILOM <tornago5000@gmail.com>
2018-10-19 00:12:20 +0300

    three

---
+++ src/HWork3.java
@@ -0,0 +1,11 @@
+public class HWork3 {
+
+    public static void main(String[] args) {
+        int a , b , c ;
+        a =15 * 100;
+        b = 600;
+        c = a - b;
+        System.out.println(c);
+    }
+
+}
