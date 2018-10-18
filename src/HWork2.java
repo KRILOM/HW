ce98d1a9186846a30751535966d45adb28e0ff8f
KRILOM <tornago5000@gmail.com>
2018-10-18 23:55:53 +0300

    two

---
+++ src/HWork2.java
@@ -0,0 +1,10 @@
+public class HWork2 {
+
+    public static void main(String[] args) {
+        int a, b , c ;
+        a = 1000;
+        b = 15*25;
+        c = a % b;
+        System.out.println(c);
+    }
+}
