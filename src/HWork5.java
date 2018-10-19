38fa2853841c4e476f8dcc3b0978ba46c40ac9bb
KRILOM <tornago5000@gmail.com>
2018-10-19 21:48:04 +0300

    five

--- src/HWork5.java
+++ src/HWork5.java
@@ -3,11 +3,18 @@ public class HWork5 {
     public static void main(String[] args) {
         double ipkred , procstav , mes;
         ipkred = 1000000.00;
-        procstav = (2.00/ 100.00);
-        mes = 2.00;
-        double koef = ((procstav * ((1 - procstav) * /* ^ */  mes))/ ((1 + procstav)* /* ^ */ mes - 1));
+        procstav = ((2.00/ 100.00)/12);
+        mes = 12.00;
+        double  partkoef1 = ((procstav) * (1 - (procstav)) );
+        System.out.println(partkoef1);
+        double partkoef12 = (1 + procstav);
+        System.out.println(partkoef12);
+        double koef = ( Math.pow(partkoef1,mes)) /  ((Math.pow(partkoef12,(mes)))-1);
+        System.out.println(koef);
         double ejemesplat = koef * ipkred;
+        System.out.println(ejemesplat);
         double stoimostpokred = mes * ejemesplat;
+        System.out.println(stoimostpokred);
         double pereplata = stoimostpokred - ipkred;
         System.out.println(pereplata);
 
