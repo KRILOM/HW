c2afdb317618ddf3bc509647636833949c9a1eec
KRILOM <tornago5000@gmail.com>
2018-10-21 11:56:47 +0300

    one

--- src/HWork1.java
+++ src/HWork1.java
@@ -1,16 +1,24 @@
+import java.util.Scanner;
 public class HWork1 {
 
     public static void main(String[] args) {
-        int a, b, c;
-
-        a = 5;//ВЫСОТА
+        //int a, b, c;
+Scanner a = new Scanner(System.in);//ВЫСОТА
+Scanner b = new Scanner(System.in);//ДЛИННА
+Scanner c = new Scanner(System.in);//ШИРИНА
+//int l;
+System.out.println( 2*((a.nextInt() * b.nextInt()) + (b.nextInt() * c.nextInt()) + (a.nextInt()*c.nextInt())));
+        /*a = 5;//ВЫСОТА
         b = 4;//ДЛИННА
         c = 2;//ШИРИНА
-        System.out.println(2 * ((a * b) + (b * c) + (a * c)));
-        if (a > c) {
-            System.out.println(a);
-        } else {
-            System.out.println(c);
+        System.out.println(2 * ((a * b) + (b * c) + (a * c)));*/
+        if (a.nextInt() > c.nextInt()) {
+            System.out.println(a.nextInt());
+        } else if (a == c ){
+            System.out.println(c.nextInt());
+        }
+        else {
+            System.out.println(c.nextInt());
         }
     }
 }
