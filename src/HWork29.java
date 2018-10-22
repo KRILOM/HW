ceda69afbb319e47c801edd1c490c5f451353738
KRILOM <tornago5000@gmail.com>
2018-10-22 17:22:39 +0300

    Homework2.9

---
+++ src/HWork29.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class HWork29 {
+    public static void main(String[] args) {
+        Scanner tar = new Scanner(System.in);
+        Scanner sred = new Scanner(System.in);
+         double  tar1 = tar.nextInt();
+         double sred1 = sred.nextInt();
+         double sum = tar1;
+     /*while(tar1>0){
+         tar1--;
+         sred1-=0.5;
+         System.out.println("Тарелок осталось: " + (sum-tar1));
+         System.out.println("Средства осталось" + sred1);
+         if(tar1 == 0){
+             System.out.println("Средство -, такрелок осталось" + tar1);
+         }
+         else{
+             System.out.println("Тарекли -, средства осталось" + sred1);
+         break;
+         }
+
+     }*/
+     /*   while(sred1>=0.5 ) {
+            sred1 -= 0.5;
+            while (tar1 >= 1) {
+                tar1 -= 1;
+            }
+                if (sred1 == 0 & tar1 != 0) {
+                    System.out.println(tar1);
+                }
+                else if (sred1 == 0 & tar1 ==0){
+                    System.out.println("Все закончилось!");
+                }
+                else{
+                    System.out.println(sred1);
+                }
+
+        }
+    */}
+}
