93dd5e07593ad417ddbfe0f335fc929150ff786a
KRILOM <tornago5000@gmail.com>
2018-10-22 11:13:47 +0300

    Homework2.3

---
+++ src/HWork23.java
@@ -0,0 +1,9 @@
+public class HWork23
+{
+    public static void main(String[] args) {
+        for (int i = 2 ; i > 0 ; i*=2){
+            System.out.println(i);
+            if (i == 1048576){break;}
+            }
+    }
+}
